e68f04c29bd2e5e9f86ba865ad11f5b788843814
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-09-02 15:03:50 +0300

    feat: подключить свайпер

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,31 @@ import "../styles/pages/index.scss";
 import "./components/parallax.js";
 import { setUpFadeInUpAnima } from "./components/fadeInUp";
 import { setUpFadeInAnima } from "./components/fadeIn";
+import Swiper, { Navigation } from "swiper";
 
-const fadeInUpArray = document.querySelectorAll('.fade-in-up');
+function setUpSwiper(sliderSelector, options) {
+	const swiper = new Swiper(sliderSelector, options);
+	return swiper;
+}
+
+const fadeInUpArray = document.querySelectorAll(".fade-in-up");
 setUpFadeInUpAnima(fadeInUpArray);
 
-const fadeInArray = document.querySelectorAll('.fade-in');
+const fadeInArray = document.querySelectorAll(".fade-in");
 setUpFadeInAnima(fadeInArray);
+
+const textSliderOptions = {
+	sliderSelector: ".roads__text-slider",
+	options: {
+		modules: [Navigation],
+		spaceBetween: 40,
+		cssMode: true,
+		mousewheel: true,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	}
+};
+
+const swiper = setUpSwiper(textSliderOptions.sliderSelector, textSliderOptions.options);
