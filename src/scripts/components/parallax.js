b46587ce70e696d175f25487f9f97fb8b3058a4a
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-09-06 17:24:31 +0300

    Завершить адаптив обложки и донастроить gsap

--- src/scripts/components/parallax.js
+++ src/scripts/components/parallax.js
@@ -3,16 +3,32 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-export function setParallaxEffect({triggerSelector, start, end}) {
-	gsap.to(triggerSelector, { 
-		y: 0, 
-		scrollTrigger: {
-			trigger: triggerSelector,
-			start: start,
-			end: end,
-			markers: false,
-			scrub: false,
-			pin: true,
-		} 
-	});
+export function setParallaxEffect({ triggerSelector, start, end }) {
+	const mm = gsap.matchMedia();
+	mm.add("(min-width: 541px)", ()=> {
+		gsap.to(triggerSelector, {
+			y: 0,
+			scrollTrigger: {
+				trigger: triggerSelector,
+				start: start,
+				end: end,
+				markers: true,
+				scrub: true,
+				pin: true,
+			},
+		});
+	})
+	mm.add("(max-width: 540px)", ()=> {
+		gsap.to(triggerSelector, {
+			y: 0,
+			scrollTrigger: {
+				trigger: triggerSelector,
+				start: "-20px center",
+				end: "20px center",
+				markers: true,
+				scrub: true,
+				pin: true,
+			},
+		});
+	})
 }
